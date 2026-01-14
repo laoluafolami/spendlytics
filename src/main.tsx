7916a949eed4e713b84a0f7dd125410b0eff2c5d
Afolami Olaoluwa <laoluafolami@outlook.com>
2026-01-14 21:02:05 +0100

    Updated index.html

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,18 @@ import { SettingsProvider } from './contexts/SettingsContext'
 import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('SW registered:', registration)
+      })
+      .catch(error => {
+        console.log('SW registration failed:', error)
+      })
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
